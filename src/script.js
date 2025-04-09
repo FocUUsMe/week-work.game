6e11e58c8a16f8d2fc66244f1d7af8fb629439a7
FocUUsMe <timoxxastolarov@gmail.com>
2025-04-09 21:44:49 +0300

    htmlAndCssUpdate

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
     const mainMenu = document.querySelector('.main-menu');
     const startBtn = document.getElementById('start_btn');
 
+    const mainGame = document.querySelector('.main-game');
+
     //События
 
     startBtn.addEventListener('click', ()=>{
         mainMenu.style.opacity = '0';
+        mainMenu.style.display = 'none';
+        mainGame.style.opacity = '1';
     });
